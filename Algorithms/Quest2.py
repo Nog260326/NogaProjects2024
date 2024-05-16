ce465b05dfa9ecf41dc51d6075a0c3dc4265868f
nogad <noga.danon123@gmail.com>
2024-05-16 23:47:22 +0300

    Algoritems in progress

--- Algorithms/Quest2.py
+++ Algorithms/Quest2.py
@@ -1,28 +1,11 @@
-def pythagorean_triplet_by_sum(sum) -> []:
-    pythagoreans = []
-    numbers = []
-    if sum <= 5:
-        return 0
-    j = 2
-    k = 3
-    for i in range(sum-3):
-        if i >sum-3:
-            return pythagoreans
-        j = i + 1
-        k = j + 1
-        for a in range(sum-4):
-            j += 1
-            k = j+1
-            for b in range(sum-5):
-                k += 1
-                if i + j + k == sum:
-                    numbers.append(i)
-                    numbers.append(j)
-                    numbers.append(k)
-                    pythagoreans.append(numbers)
-                numbers = []
-    return pythagoreans
+def pythagorean_triplet_by_sum(sum : int) -> bool:
+    for a in range(0, sum + 1):
+        for b in range(0, sum + 1):
+            for c in range(0, sum + 0):
+                if (a + b + c) == sum and (a ** 2 + b ** 2) == (c ** 2) and a < b < c:
+                    print(a, " < ", b, " < ", c)
 
 
 if __name__ == '__main__':
-    print(pythagorean_triplet_by_sum(8))
+    sum = 30
+    pythagorean_triplet_by_sum(sum)
--- Algorithms/Quest3.py
+++ Algorithms/Quest3.py
@@ -1,19 +1,47 @@
 def is_sorted_polyndrom(word) -> bool:
-    stack = []
-    stack2 = []
-    for i in range(len(word)-2):
-        if not word[i] == word[i+2]:
-            stack.append(word[i])
-    middle = i+1
-    for i in range(int(len(word) / 2)-1):
-        stack2.append(word[middle])
-        middle += 1
-    for i in range(len(stack)):
-        if not stack[i] == stack2[i]:
-            return False
-    return True
+    word_chars = []
+    for i in range(len(word)):
+        word_chars.append(word[i])
+    first_part = []
+    last_part = []
+    middle = ""
+    is_sorted = 0
+    is_short_sorted = 0
+    beginning_len = len(word_chars)
+    while not len(word_chars) < beginning_len / 2 + 1:
+        if not word_chars[0] == word_chars[2] and middle == "":
+            if word_chars[0] < word_chars[1]:
+                is_sorted += 1
+            first_part.append(word_chars[0])
+            del word_chars[0]
+        elif middle == "":
+            if word_chars[0] < word_chars[1]:
+                is_sorted += 1
+            first_part.append(word_chars[0])
+            middle = word_chars[1]
+            del word_chars[0]
+            del word_chars[0]
+    while not len(word_chars) == 0 and not middle == "":
+        last_part.append(word_chars[len(word_chars) - 1])
+        del word_chars[len(word_chars) - 1]
+    if not len(last_part) == len(first_part):
+        return False
+    is_polyndrom = 0
+    for i in range(len(first_part)):
+        if first_part[i] == last_part[i]:
+            is_polyndrom += 1
+    if is_sorted == 1 and len(word) == 3:
+        is_short_sorted = 1
+        is_sorted = 1
+    if is_polyndrom == len(first_part) and is_sorted == int(len(word) / 2):
+        if is_sorted == 0 and is_short_sorted == 1:
+            return True
+        if is_sorted > 0:
+            return True
+    return False
 
 
 if __name__ == '__main__':
-    print(is_sorted_polyndrom("abcdcba"))
+    word = "abcdcba"
+    print(is_sorted_polyndrom(word))
 
